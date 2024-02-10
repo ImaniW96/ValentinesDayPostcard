f6cd7e196140c2e3c2051562695f3cdfebf95fbb
ImaniW96.git <imaniwalker0305@gmail.com>
2024-02-09 19:17:14 -0500

    Read me update

---
+++ README.md
@@ -0,0 +1,31 @@
+# TriplleTen Valentines Day Challenge: Valentines Day Card
+
+### Overview
+
+- Intro
+
+- Project Design
+
+- Images
+
+**Intro**
+
+This project is made so all the elements are displayed correctly on popular screen sizes. We recommend investing more time in completing this project, since it's more difficult than previous ones.
+
+**Project Design**
+
+For the envelope design, I found multiple design inspirations on youtube.
+
+- [Link to the envelope design on Youtube]https://www.youtube.com/watch?v=q7F_0WJJD7g
+
+**Images**
+All Images were found on Unplash
+
+** About this project**
+TripleTen Valentines Day Challenge: Valentines day Card
+
+This project showcasts a love letter which is opened by clicking the envelope
+this poem is a showcast of love you may feel for your partner friend or family.
+
+In this project with the help of media queries, every element was configured to display correctly on popular screen sizes.
+Link to project: https://github.com/ImaniW96/ValentinesDayPostcard
---
+++ index.js
@@ -0,0 +1,46 @@
+const openLetter = document.getElementById("open-letter");
+
+// Add a click event listener
+openLetter.addEventListener("click", () => {
+  this._openLetter();
+});
+
+export default class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = document.querySelector(popupSelector);
+
+    this._popupCloseButton = this._popupElement.querySelector(".modal__close");
+  }
+
+  open() {
+    this._popupElement.classList.add("modal_opened");
+
+    document.addEventListener("keydown", this._handleEscapeClose);
+  }
+  close() {
+    this._popupElement.classList.remove("modal_opened");
+
+    document.removeEventListener("keydown", this._handleEscapeClose);
+  }
+
+  // _handleEscapeClose = (evt) => {
+
+  //   if (evt.key === "Escape") {
+
+  //     this.close();
+
+  //   }
+
+  // };
+
+  setEventListeners() {
+    this._popupElement.addEventListener("click", (evt) => {
+      if (
+        evt.target === this._popupCloseButton ||
+        evt.target === this._popupElement
+      ) {
+        this.close();
+      }
+    });
+  }
+}
